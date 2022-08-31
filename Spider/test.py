55dba0dbcefdd5d553b86696c9430559318cdff9
swheart <zhuxiaowei123456@qq.com>
2022-08-31 15:41:08 +0800

    Update test.py

--- Spider/test.py
+++ Spider/test.py
@@ -7,68 +7,57 @@ import time
 from selenium.webdriver.chrome.options import Options
 import os
 
-def submit():
-    try:
-        # driver = webdriver.Chrome(executable_path='/Usr/local/bin/ChromeDriver')
 
 
 
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chromedriver = "/Usr/local/bin/ChromeDriver"
-        os.environ["webdriver.chrome.driver"] = chromedriver
-        driver = webdriver.Chrome(options=chrome_options, executable_path=chromedriver)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chromedriver = "/Usr/local/bin/ChromeDriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(options=chrome_options, executable_path=chromedriver)
 
 
-        url = r'http://got.goermicro.com:8089/got/temperature/attend.html'
-        driver.get(url)
+url = r'http://got.goermicro.com:8089/got/temperature/attend.html'
+driver.get(url)
 
-        print("访问页面成功")
+print("访问页面成功")
 
-        username = driver.find_element(by='id', value='employeeNumber')
-        StudentID = '9002370'
-        username.send_keys(StudentID)
+username = driver.find_element(by='id', value='employeeNumber')
+StudentID = '9002370'
+username.send_keys(StudentID)
 
-        print("填报员工号成功")
+print("填报员工号成功")
 
-        secretKey = driver.find_element(by='id', value='numId')
-        KeyNum = '041815'
-        secretKey.send_keys(KeyNum)
+secretKey = driver.find_element(by='id', value='numId')
+KeyNum = '041815'
+secretKey.send_keys(KeyNum)
 
-        print("填报身份证成功")
+print("填报身份证成功")
 
-        logbtn = driver.find_element(by='id', value='sub')
-        logbtn.click()
+logbtn = driver.find_element(by='id', value='sub')
+logbtn.click()
 
-        print("提交员工信息成功")
-        time.sleep(1)
+print("提交员工信息成功")
+time.sleep(1)
 
-        logbtn = driver.find_element(by='id', value='agreement')
-        logbtn.click()
+logbtn = driver.find_element(by='id', value='agreement')
+logbtn.click()
 
-        print("同意协议")
-
-
-        time.sleep(1)
+print("同意协议")
 
 
-        driver.get('http://got.goermicro.com:8089/got/temperature/to_temperature.html')
-
-        print("进入体温填报页面成功")
+time.sleep(1)
 
 
-        time.sleep(50)
+driver.get('http://got.goermicro.com:8089/got/temperature/to_temperature.html')
+
+print("进入体温填报页面成功")
+print(driver.title)
+
+time.sleep(50)
 
 
-
-    except Exception as e:
-        print("填报失败")
-        pass
-
-
-if __name__ == '__main__':
-    submit()
 
