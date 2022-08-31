77383b5ca5142fa8429bb8c9b014d952bcf84c15
swheart <zhuxiaowei123456@qq.com>
2022-08-31 14:42:24 +0800

    1

--- Spider/test.py
+++ Spider/test.py
@@ -1,15 +1,50 @@
+# -*- coding:utf-8 -*-
+import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
-driver.get("https://www.baidu.com")
-print(driver.title)
-driver.quit()
+
+def submit():
+    try:
+        driver = webdriver.Chrome(executable_path='/Usr/local/bin/ChromeDriver')
+
+        url = r'http://got.goermicro.com:8089/got/temperature/attend.html'
+        driver.get(url)
+
+        username = driver.find_element(by='id', value='employeeNumber')
+        StudentID = '9002370'
+        username.send_keys(StudentID)
+
+        secretKey = driver.find_element(by='id', value='numId')
+        KeyNum = '041815'
+        secretKey.send_keys(KeyNum)
+
+        logbtn = driver.find_element(by='id', value='sub')
+        logbtn.click()
+
+        time.sleep(1)
+
+        logbtn = driver.find_element(by='id', value='agreement')
+        logbtn.click()
+
+
+        time.sleep(1)
+
+
+        driver.get('http://got.goermicro.com:8089/got/temperature/to_temperature.html')
+
+
+        time.sleep(50)
+
+
+
+    except Exception as e:
+        alert("填报失败")
+        pass
+
+
+if __name__ == '__main__':
+    submit()
+
